886bad31463444cb0ecf7812aaeb89a55a5dd5c8
Prabhu161292 <41663693+Prabhu161292@users.noreply.github.com>
2018-07-25 13:43:13 +0530

    Test2
    Second Exercise

---
+++ second.rb
@@ -0,0 +1,7 @@
+#Comments are very important in your programs.They are used to tell  you what something does inEnglish,and they are used to disable parts of your program if you need to remove them temporarily.
+puts "The Second exercise is about the Comments,if # is used in beginning of the statement then it turns to Comment"
+#The primary difference between the puts and print is, puts leaves a newline after executing whether print doesnot
+print "No newline "
+#Left a space on the above statement
+puts "Newline"
+puts "End"
